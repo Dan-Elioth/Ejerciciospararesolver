6257034bf35747c5693697f5701d403117cd9d1e
eliothC <>
2021-05-05 21:38:49 +0000

    Ejercicios

---
+++ Tareaejercicios/Example3.py
@@ -0,0 +1,16 @@
+#Ejercicio 3.3
+print("Buen Día")
+#proceso de eleccion
+saldo=int(input("¿Cuanto dinero dispone para su compra? "))
+#proceso
+opciones=""
+if saldo<=10.0:
+  opciones="tarjeta"
+elif saldo>=11.0 and saldo<=100.0:
+  opciones="tarjeta o chocolate"
+elif saldo>=101.0 and saldo<=250.0:
+  opciones="tarjeta, chocolate o flores"
+else:
+  opciones="tarjeta, chocolate, flores o un anillo"
+#datos de salida
+print("puede adquirir los siguientes elementos:", opciones) 
